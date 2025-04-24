05f549032cac7c5264c5857f2165dc22f2700581
ahrizmo <ahrizmo@gmail.com>
2025-04-24 12:20:37 +0200

    l

--- ProjetNarouto/code/jeu_de_cartes/deck/decks.py
+++ ProjetNarouto/code/jeu_de_cartes/deck/decks.py
@@ -1,11 +1,11 @@
 from card import Card
 from enumeration import *
 # Création des cartes
-c1 = Card("Dragon de feu", TypeCard.Ninja, [6,0,0], [8,0,0], ElementalChakra.Fire, 5)
-c2 = Card("Chevalier", TypeCard.Ninja, [4,0,0], [2,0,0], ElementalChakra.Earth, 3)
-c3 = Card("Mage de glace", TypeCard.Ninja, [4,0,0], [5,0,0], ElementalChakra.Water, 4)
-c4 = Card("Mercenaire", TypeCard.Ninja, [2,0,0], [2,0,0], ElementalChakra.Lightning, 2)
-c5 = Card("soldat", TypeCard.Ninja, [1,0,0], [1,0,0], ElementalChakra.Wind, 1)
+c1 = Card("Dragon de feu", TypeCard.Ninja, [6,0,0], [8,0,0], ElementalChakra.Fire, 5,"test")
+c2 = Card("Chevalier", TypeCard.Ninja, [4,0,0], [2,0,0], ElementalChakra.Earth, 3,"test")
+c3 = Card("Mage de glace", TypeCard.Ninja, [4,0,0], [5,0,0], ElementalChakra.Water, 4,"test")
+c4 = Card("Mercenaire", TypeCard.Ninja, [2,0,0], [2,0,0], ElementalChakra.Lightning, 2,"test")
+c5 = Card("soldat", TypeCard.Ninja, [1,0,0], [1,0,0], ElementalChakra.Wind, 1,"test")
 
 # Decks prédéfinis pour les joueurs
 deck1 = [c1, c2,c3,c4,c5]
